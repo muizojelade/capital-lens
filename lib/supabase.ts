8149929a694dc36bf64790427a4c468d92ba98e3
Muiz Ojelade <muizojelade@gmail.com>
2026-08-22 03:21:03 +0100

    debugged RLS error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
+import { createBrowserClient } from "@supabase/ssr";
 
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
 );
